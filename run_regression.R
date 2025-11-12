c0b0229217507114f1d59603a0e92909349d68e2
toddkawahara <toddkawahara@hotmail.com>
2025-11-11 20:23:59 -0800

    adjust plots, add 17311+17313 regression

--- run_regression.R
+++ run_regression.R
@@ -40,6 +40,17 @@ df_billing_services_17312 <- df_billing_services %>% filter(HCPCS_Cd == 17312)
 df_billing_services_17313 <- df_billing_services %>% filter(HCPCS_Cd == 17313)
 df_billing_services_17314 <- df_billing_services %>% filter(HCPCS_Cd == 17314)
 df_billing_services_all <- df_billing_services %>% distinct(Billing_Year, total_services, total_surgeons, total_avg_services)
+df_billing_services_17311_17313 <- df_billing_services %>% filter(HCPCS_Cd == 17311 | HCPCS_Cd == 17313) %>%
+  select(-c(total_services, code_avg_services, total_avg_services)) %>%
+  group_by(Billing_Year, total_surgeons) %>%
+  summarize(code_services = sum(code_services)) %>%
+  ungroup() %>%
+  mutate(code_avg_services = code_services/total_surgeons, 
+         HCPCS_Cd = '17311_17313') %>%
+  select(HCPCS_Cd, everything())
+
+# save 17311+17313 df
+write.csv(df_billing_services_17311_17313, 'df_billing_services_17311_17313.csv', row.names = FALSE)
 
 # running regressions for codes
 model_17311 <- lm(code_avg_services ~ Billing_Year, data = df_billing_services_17311)
@@ -47,6 +58,7 @@ model_17312 <- lm(code_avg_services ~ Billing_Year, data = df_billing_services_1
 model_17313 <- lm(code_avg_services ~ Billing_Year, data = df_billing_services_17313)
 model_17314 <- lm(code_avg_services ~ Billing_Year, data = df_billing_services_17314)
 model_all <- lm(total_avg_services ~ Billing_Year, data = df_billing_services_all)
+model_17311_17313 <- lm(code_avg_services ~ Billing_Year, data = df_billing_services_17311_17313)
 
 # model summaries
 summary(model_17311)
@@ -54,6 +66,10 @@ summary(model_17312)
 summary(model_17313)
 summary(model_17314)
 summary(model_all)
+summary(model_17311_17313)
+
+# ---- plots ----
+
 
 # individual trend plot labels
 p_values = c(0.951, 0.011, 0.007, 0.835)
@@ -63,7 +79,25 @@ label_positions <- df_billing_services %>%
   mutate(p_values = p_values) %>%
   select(HCPCS_Cd, Billing_Year, code_avg_services, p_values)
 
-# individual trend plot
+# total trend plot labels
+label_positions_total <- df_billing_services %>%
+  slice(n()) %>%
+  select(Billing_Year, total_avg_services) %>%
+  mutate(p_values = 0.391, 
+         HCPCS_Cd = 'Total') %>%
+  rename(code_avg_services = total_avg_services)
+
+# combine labels
+label_positions_combined = rbind(label_positions, label_positions_total)
+
+# combine total and individual code data
+df_billing_services_all <- df_billing_services_all %>%
+  mutate(HCPCS_Cd = 'Total', 
+         code_services = total_services, 
+         code_avg_services = total_avg_services)
+df_billing_services <- rbind(df_billing_services, df_billing_services_all)
+
+#  trend plot
 ggplot(df_billing_services, 
        aes(x = Billing_Year, 
            y = code_avg_services, 
@@ -77,9 +111,10 @@ ggplot(df_billing_services,
               linewidth = 0.825
   ) +
   geom_point() +
-  geom_text(data = label_positions, 
-            aes(label = paste0('trend p-value: ', p_values)), 
-            hjust = 1, vjust = -2, size = 3.2
+  geom_text(data = label_positions_combined,
+            aes(label = paste0('trend p-value: ', p_values)),
+            hjust = 1, vjust = -1, size = 3.2,
+            color = 'black'
   ) +
   theme_classic() +
   labs(
@@ -87,20 +122,30 @@ ggplot(df_billing_services,
     y = "Services",
     x = "Year",
     color = "Code"
-  )
+  ) +
+  scale_color_manual(
+    values = c(
+      "Total" = 'black', 
+      "17311" = 'red', 
+      "17312" = 'blue', 
+      "17313" = 'orange', 
+      "17314" = 'darkgreen'
+    )
+  ) +
+  scale_y_continuous(limits = c(0, 600))
 
-# total trend plot labels
-label_positions_total <- df_billing_services %>%
+# 17311 17313 trend plot labels
+label_positions_17311_17313 <- df_billing_services_17311_17313 %>%
   slice(n()) %>%
-  select(Billing_Year, total_avg_services) %>%
-  mutate(p_value = 0.391)
+  select(Billing_Year, code_avg_services) %>%
+  mutate(p_value = 0.455)
 
-# total trend plot
-ggplot(df_billing_services %>% distinct(Billing_Year, total_avg_services), 
+# 17311 17313 trend plot
+ggplot(df_billing_services_17311_17313, 
        aes(x = Billing_Year, 
-           y = total_avg_services
+           y = code_avg_services
        )
-  ) + 
+) + 
   geom_line(linewidth = 0.9) + 
   geom_smooth(method = 'lm', 
               color = 'black',
@@ -109,14 +154,15 @@ ggplot(df_billing_services %>% distinct(Billing_Year, total_avg_services),
               linewidth = 0.825
   ) +
   geom_point() +
-  geom_text(data = label_positions_total, 
+  geom_text(data = label_positions_17311_17313, 
             aes(label = paste0('trend p-value: ', p_value)), 
-            hjust = 1, vjust = 10, size = 3.2
+            hjust = 1, vjust = -1, size = 3.2
   ) +
   theme_classic() +
   labs(
-    # title = "Average Total Services per Year",
+    # title = "17311 and 17313 Code Avg Services per Year",
     y = "Services",
     x = "Year",
     color = "Code"
-  )
+  ) +
+  scale_y_continuous(limits = c(0, 600))
